cb2784aee922abb67a2c3600d5c15035be2571c3
RetroMartin <martin@dreamweb.space>
2020-02-15 12:24:32 +0000

    delete

--- sandbox.js
+++ sandbox.js
@@ -2,23 +2,42 @@ const list=document.querySelector('ul');
 const form=document.querySelector('form');
 
 
-const addRecipe = (recipe) => {
-    console.log(recipe.created_at);
+const addRecipe = (recipe,id) => {
+    console.log(recipe);
+    //let time = recipe.created_at.toDate();
     let html = `
-    <li>
+    <li data-id="${id}">
         <div>${recipe.title}</div>
+        <button class="btn btn-danger btn-sm my-2">Delete</button>
         
     </li>
     `;
     list.innerHTML +=html;
 }
-// get documents
-db.collection('recipes').get().then((snapshot) => {
-    //when we have data
-    snapshot.docs.forEach(doc => {
-        console.log(doc.data());
-        addRecipe(doc.data());
-    });
+
+// add documents
+form.addEventListener('submit', e => {
+    e.preventDefault();
+//    const now = new Date();
+    const recipe = {
+        title: form.recipe.value
+
+    };
+
+db.collection('recipes').add(recipe).then(()=> {
+    console.log('recipe added');
 }).catch(err=>{
-    console.log(err);
+        console.log(err);
+
+})
+
+});
+//delete
+list.addEventListener('click', e=>{
+    if (e.target.tagName==='BUTTON'){
+        const id=e.target.parentElement.getAttribute('data-id');
+        db.collection('recipes').doc(id).delete().then(()=>{
+            console.log('rec deleted')
+        })
+    }
 })
